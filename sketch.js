319a9d8e1493b3de3d39fa4bc21afa583779e121
Ronit2008 <70698780+Ronit2008@users.noreply.github.com>
2021-04-21 17:02:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var drops = [];
+var maxDrops = 100;
+var umbrella, rand, night;
+var thunder, thunder1, thunder2, thunder3, thunder4;
+var thunderCreatedFrame = 0;
+
+
+function preload(){
+ night = loadImage("images/rainynight.jpg");
+ thunder1 = loadImage("images/thunderbolt/1.png");
+ thunder2 = loadImage("images/thunderbolt/2.png");
+ thunder3 = loadImage("images/thunderbolt/3.png");
+ thunder4 = loadImage("images/thunderbolt/4.png");
+}
+
+function setup(){
+   var canvas = createCanvas(500, 700);
+ engine = Engine.create()
+
+ world = Engine.world;
+
+ umbrella = new Umbrella(200,500);
+ for (var i = 0; i < maxDrops; i++){
+ drops.push(new createDrops(random(0,500), random(0,500)));
+ }
+    
+}
+
+function draw(){
+Engine.update(engine);
+background(night)
+
+
+
+rand = Math.round(random(1,4))
+if(frameCount%80 === 0) {
+  thunderCreatedFrame = frameCount;
+  Thunder = createSprite(random(10,370), random(10,30), 10 , 10);
+  switch(rand){
+    case 1: thunder.addImage(thunder1);
+    break;
+    case 2: thunder.addImage(thunder2);
+    break;
+    case 3: thunder.addImage(thunder3);
+    break;
+    case 4: thunder.addImage(thunder4);
+    break;
+    default:break;
+  }
+Thunder.scale = 0.7
+
+}
+if (thunderCreatedFrame + 10 === frameCount && Thunder){
+  Thunder.destroy();
+}
+
+Umbrella.display();
+
+for(var i = 0; i>maxDrops;i++){
+  drops[i].display();
+  drops[i].update();
+}
+
+drawSprites();
+
+}   
